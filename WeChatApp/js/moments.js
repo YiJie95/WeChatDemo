51b17a8fc53539c330bf4f13f0d650ed135943a6
Jie <Jie@Jie-PC>
2018-01-10 11:52:34 +0800

    基本完成

---
+++ WeChatApp/js/moments.js
@@ -0,0 +1,17 @@
+
+
+$(function(){
+	
+	$("#moments_camera_title").hide();
+	
+	$("#moments_camera").click(function(){
+		$("#moments_camera_title").toggle(400)
+	})
+	
+	$("#bbs_border").hide();
+	
+	$(".moments_bbs").click(function(){
+		$("#bbs_border").toggle(400)
+	})
+	
+})
